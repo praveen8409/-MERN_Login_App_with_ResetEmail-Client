c1173a5f3e1c2eb941578b817afb16020343be4a
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2024-01-12 12:58:48 +0530

    Implement password Reset component

--- src/helper/validate.js
+++ src/helper/validate.js
@@ -14,6 +14,17 @@ export async function passwordValidate(values){
     return errors;
 }
 
+/** validate reset password */
+export async function resetPasswordValidation(values){
+    const errors = passwordVerify({}, values);
+
+    if(values.password !== values.confirm_pwd){
+        errors.exist = toast.error("Password not match...!");
+    }
+
+    return errors;
+}
+
 //************************************************************************ */
 
 
