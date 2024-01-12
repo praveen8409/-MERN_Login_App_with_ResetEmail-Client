00a8b4ab296e868de9654db2340a8e172c8de122
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2024-01-12 12:47:37 +0530

    Implement Password component

--- src/helper/validate.js
+++ src/helper/validate.js
@@ -7,6 +7,15 @@ export async function usernameValidate(values){
     return errors;
 }
 
+/** validate password */
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values);
+
+    return errors;
+}
+
+//************************************************************************ */
+
 
 /** validate username */
 function usernameVerify(error = {}, values){
@@ -17,4 +26,22 @@ function usernameVerify(error = {}, values){
     }
 
     return error;
+}
+
+/** validate password */
+function passwordVerify(errors = {}, values){
+
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if(!values.password){
+        errors.password = toast.error("Password Required...!");
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password...!");
+    }else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 characters long");
+    }else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special character");
+    }
+
+    return errors;
 }
